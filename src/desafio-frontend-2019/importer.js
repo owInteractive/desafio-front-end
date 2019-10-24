25689cab89fb2bf51314dd22cff4af20de7de301
Waller Jhonatan <waller.jhonatanc@gmail.com>
2019-10-24 08:13:02 -0300

    Add importer on main

---
+++ src/desafio-frontend-2019/importer.js
@@ -0,0 +1,3 @@
+import '@/registerServiceWorker'
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css'
--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
+import '@/desafio-frontend-2019'
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import './registerServiceWorker'
-
-import 'bootstrap'; import 'bootstrap/dist/css/bootstrap.min.css'
 
 Vue.config.productionTip = false
 
