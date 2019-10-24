5da374aa6ee62ab5981fa0ba146ac8ff8b4a90bc
Waller Jhonatan <waller.jhonatanc@gmail.com>
2019-10-24 09:33:12 -0300

    add configuration variables

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import '@/desafio-frontend-2019'
 import Vue from 'vue'
+import VueConfig from 'vue-config'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Config from './config'
 
 Vue.config.productionTip = false
+Vue.use(VueConfig, Config)
 
 new Vue({
   router,
