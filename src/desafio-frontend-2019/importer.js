df2a4d7c464528707af25bf3509e46f8a67e794f
Waller Jhonatan <waller.jhonatanc@gmail.com>
2019-10-24 09:34:54 -0300

    Import bootstrap-vue and vue-axios

--- src/desafio-frontend-2019/importer.js
+++ src/desafio-frontend-2019/importer.js
@@ -1,3 +1,13 @@
 import '@/registerServiceWorker'
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css'
+
+import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import BootstrapVue from 'bootstrap-vue'
+
+require('bootstrap')
+
+Vue.use(VueAxios, axios)
+Vue.use(BootstrapVue)
