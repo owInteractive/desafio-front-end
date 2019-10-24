88fd05be35a4088225e88d4ea9b952a7755def3b
Waller Jhonatan <waller.jhonatanc@gmail.com>
2019-10-24 07:43:59 -0300

    Includes Gulp and configure tasks

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var sass = require('gulp-sass');
+var scssFiles = './src/assets/scss/style.scss';
+const assetPath = './src/assets/'
+
+gulp.task('sassdev', function () {
+  return gulp.src(assetPath + 'scss/style.scss')
+      .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+      .pipe(concat('style.css'))
+      .pipe(sass({outputStyle: 'expanded'}))
+      .pipe(gulp.dest(assetPath + '/css'))
+});
+
+gulp.task('sassprod', function () {
+  return gulp.src(assetPath + 'scss/style.scss')
+      .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+      .pipe(concat('style.min.css'))
+      .pipe(sass({outputStyle: 'compressed'}))
+      .pipe(gulp.dest(assetPath + '/css'));
+});
+
+gulp.task('default', function () {
+  gulp.watch(scssFiles, gulp.series(['sassdev', 'sassprod']));
+});
+
+
+
