e44c0bc828c137ad3a290cc852674e5ea5bd2843
Waller Jhonatan <waller.jhonatanc@gmail.com>
2019-10-25 13:08:13 -0300

    import toast and  vue-bootstrap

--- src/desafio-frontend-2019/importer.js
+++ src/desafio-frontend-2019/importer.js
@@ -1,16 +1,35 @@
 import '@/registerServiceWorker'
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css'
 import '@/assets/css/style.css'
+import 'vue-toastr-2/dist/vue-toastr-2.min.css'
+import 'bootstrap/dist/css/bootstrap-grid.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 import Vue from 'vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
-import BootstrapVue from 'bootstrap-vue'
 import Theme from '@/components/template/Theme'
+import VueToastr2 from 'vue-toastr-2'
+import BootstrapVue from 'bootstrap-vue'
 
-require('bootstrap')
+const toastrConfigs = {
+  closeButton: true,
+  debug: false,
+  progressBar: true,
+  positionClass: 'toast-top-right',
+  onclick: null,
+  showDuration: '300',
+  hideDuration: '1000',
+  timeOut: '5000',
+  extendedTimeOut: '1000',
+  showEasing: 'swing',
+  hideEasing: 'linear',
+  showMethod: 'fadeIn',
+  hideMethod: 'fadeOut'
+}
+
+window.toastr = require('toastr')
 
 Vue.component('Theme', Theme)
 Vue.use(VueAxios, axios)
+Vue.use(VueToastr2, toastrConfigs)
 Vue.use(BootstrapVue)
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import cart from './modules/cart'
+import createPersistedState from 'vuex-persistedstate'
 
 Vue.use(Vuex)
-
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
+  plugins: [createPersistedState({storage: window.localStorage})],
   modules: {
+    cart,
   }
 })
