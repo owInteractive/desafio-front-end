813f621f688475ad203919076d870e7a4f53ad71
Waller Jhonatan <waller.jhonatanc@gmail.com>
2019-10-26 18:23:11 -0300

    Imports sweetalert2 vee-validate vue-blockui vue-the-mask

--- src/desafio-frontend-2019/importer.js
+++ src/desafio-frontend-2019/importer.js
@@ -2,6 +2,7 @@ import '@/registerServiceWorker'
 import '@/assets/css/style.css'
 import 'vue-toastr-2/dist/vue-toastr-2.min.css'
 import 'bootstrap/dist/css/bootstrap-grid.css'
+import 'sweetalert2/dist/sweetalert2.min.css';
 
 import Vue from 'vue'
 import axios from 'axios'
@@ -9,6 +10,22 @@ import VueAxios from 'vue-axios'
 import Theme from '@/components/template/Theme'
 import VueToastr2 from 'vue-toastr-2'
 import BootstrapVue from 'bootstrap-vue'
+import VueTheMask from 'vue-the-mask'
+import VueSweetalert2 from 'vue-sweetalert2'
+import {ValidationProvider} from 'vee-validate';
+import br from 'vee-validate/dist/locale/pt_BR'
+import VueEvents from 'vue-events'
+import BlockUI from 'vue-blockui'
+
+import {extend} from 'vee-validate';
+import * as rules from 'vee-validate/dist/rules';
+
+for (let rule in rules) {
+  extend(rule, {
+    ...rules[rule],
+    message: br.messages[rule]
+  });
+}
 
 const toastrConfigs = {
   closeButton: true,
@@ -28,7 +45,12 @@ const toastrConfigs = {
 
 window.toastr = require('toastr')
 
+Vue.component('ValidationProvider', ValidationProvider);
 Vue.component('Theme', Theme)
 Vue.use(VueAxios, axios)
 Vue.use(VueToastr2, toastrConfigs)
 Vue.use(BootstrapVue)
+Vue.use(VueTheMask)
+Vue.use(VueSweetalert2, {confirmButtonColor: '#716ACA'})
+Vue.use(VueEvents)
+Vue.use(BlockUI)
