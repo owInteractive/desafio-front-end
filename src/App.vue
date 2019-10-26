dba3401fdba074e31319558bc3518187181f7538
Waller Jhonatan <waller.jhonatanc@gmail.com>
2019-10-26 18:28:23 -0300

    Include BlockUi on App

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,35 @@
 <template>
     <div id="app">
+        <BlockUI v-if="display" style="width: 30px">
+            <div align="center">
+                <img src="@/assets/logo.png" alt="" width="60">
+                <div class="loader"></div>
+            </div>
+        </BlockUI>
         <theme>
             <router-view/>
         </theme>
     </div>
 </template>
+<script>
+  export default {
+    data () {
+      return {
+        display: false,
+      }
+    },
+    created () {
+      let self = this
+      this.$events.on('DisplayBlockUI', eventData => self.displayBlockUI(eventData))
+    },
+    methods: {
+      displayBlockUI (evt) {
+        this.display = evt
+      }
+    }
+  }
+
+</script>
 
 <style>
     #app {
